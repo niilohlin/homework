1c2b7cd5797b4dca28a7750597507db751f11e8c
Niil Öhlin <niil.94@hotmail.com>
2013-09-14 12:35:59 +0200

    almost done with koch

--- labbar/lab4/telefonbok.py
+++ labbar/lab4/telefonbok.py
@@ -7,22 +7,31 @@ prompt = "phonebook> "
 class NoFunction(Exception):
 	pass
 
+def parse(text, pbook):
+	elements = text.split(' ')
+	function = elements[0]
+	args = elements[1:len(elements)]
+	if not function in ['add', 'alias', 'change', 'save', 'load',
+			'lookup', 'save', 'quit']:
+		raise NoFunction
+
+	resbook = pbook
+	if function == 'add':
+		resbook[elements[0]] = elements[1]
+		return resbook
+	elif function == 'lookup':
+		return pbook[elements[0]]
+	elif function == 'alias':
+		resbook[elements[1]] = pbook[elements[0]]
+		return resbook
+	elif function == 'change':
+		resbook[
+
 def main():
 	inp = ''
 	while not inp == "quit":
 		try:
 			inp = raw_input(prompt)
-			elements = inp.split(' ')
-			function = elements[0]
-			args = elements[1:len(elements)]
-			if not function in ['add', 'alias', 'change', 'save', 'load',
-					'lookup', 'save', 'quit']:
-				raise NoFunction
-			if function == 'add':
-				phonebook[elements[0]] = elements[1]
-			elif function == 'lookup':
-				print phonebook[elements[0]]
-			elif function == '
 		except(NoFunction):
 			print 'no such function'
 
