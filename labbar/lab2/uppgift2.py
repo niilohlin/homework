86fcc680ca0f1fdae99e5c77d6774a1cdadc9ab3
Niil Ohlin <niil.94@hotmail.com>
2013-09-24 13:45:25 +0200

    fixed

--- labbar/lab2/uppgift2.py
+++ labbar/lab2/uppgift2.py
@@ -2,12 +2,17 @@
 # -*- coding: ascii -*-
 
 # This function is tail call optimized
-def digit_sum(n, acc=0):
-	"""Return the digit sum of a number"""
+#def digit_sum(n, acc=0):
+#	"""Return the digit sum of a number"""
+#	if n == 0:
+#		return acc
+#	else:
+#		return digit_sum(n / 10, acc + n % 10)
+def digit_sum(n):
 	if n == 0:
-		return acc
+		return 0
 	else:
-		return digit_sum(n / 10, acc + n % 10)
+		return n % 10 + digit_sum(n / 10)
 
 def digit_sum2(n):
 	s = 0
