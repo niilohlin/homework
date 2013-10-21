4fbebd4c73432cd2d00f320e69f6eaf2745af679
Niil Öhlin <niil.94@hotmail.com>
2013-10-21 18:03:16 +0200

    all keywords working

--- labbar/lab4/telefonbok.py
+++ labbar/lab4/telefonbok.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import parser
+import sys
 
 class Entry:
+    """Entry, name + number where name and number is a string"""
     def __init__(self, name, number):
-        self.aliaslist = []
-        self.aliaslist.append(name)
+        self.aliaslist = [name]
         self.number = number
     
     def alias(self, alias):
@@ -16,41 +17,97 @@ class Entry:
         self.number = newnumber
 
 def get(pbook, name):
+    """Loop through the phonebook in order.
+    Return Entry if found. Else None
+    """
     for entry in pbook:
         for alias in entry.aliaslist:
             if name == alias:
                 return entry
     
 
+def if_in(fun):
+    def new(*args):
+        if get(args[0], args[1]):
+            return fun(*args)
+        else:
+            return not_found % args[0]
+    return new
+
+not_found = "%s not in phonebook"
 def add(pbook, name, number):
     pbook.append(Entry(name, number))
 
 def alias(pbook, name, newname):
-    get(pbook, name).alias(newname)
+    if not get(pbook, name) == None:
+        get(pbook, name).alias(newname)
+    else:
+        return not_found % name
 
 def lookup(pbook, name):
     temp = get(pbook, name)
     if not temp == None:
         return "%s has phonenumber %s" %(name, temp.number)
     else:
-        return "%s not in phonebook" % name
+        return not_found % name
+
+def change(pbook, name, number):
+    if get(pbook, name):
+        get(pbook, name).number = number
+    else:
+        return not_found % name
+        
+def save(pbook, filename):
+    f = open(filename, 'w')
+    for entry in pbook:
+        f.write(entry.number + ";")
+        for alias in entry.aliaslist:
+            f.write(alias + ";")
+    f.close()
+    
+def load(filename):
+    """Assume perfect filetype"""
+    f = open(filename, 'r')
+    entries = f.readlines()
+    pbook = []
+    for entry in entries:
+        values = etry.split(';')
+        e = Entry(number=values[0], name=values[1])
+        if len(values) > 2:
+            for i in range(len(values) - 2):
+                e.alias(values[i + 2])
+        pbook.append(e)
+    
+    return pbook
     
 def exe(pbook, tree):
-    
+    """execute a tree i.e a list of statements which is
+    a list of keywords and identifiers
+    """
+
     succ = True
     res = ""
     for statement in tree:
         if statement[0] == parser.Keyword("add"):
             add(pbook, statement[1], statement[2])
         elif statement[0] == parser.Keyword("alias"):
-            alias(pbook, statement[1], statement[2])
+            res = alias(pbook, statement[1], statement[2])
         elif statement[0] == parser.Keyword("lookup"):
             res = lookup(pbook, statement[1])
+        elif statement[0] == parser.Keyword("change"):
+            res = change(pbook, statement[1], statement[2])
+        elif statement[0] == parser.Keyword("save"):
+            save(pbook, statement[1])
+        elif statement[0] == parser.Keyword("load"):
+            pbook = load(statement[1])
+        elif statement[0] == parser.Keyword("quit"):
+            sys.exit(0)
             
+
         if not res == '':
             print res
             res = ''
-           
+
 prompt = "phonebook"
 def main():
     pbook = []
